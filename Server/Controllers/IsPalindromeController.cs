9a4748ab42d0a858f5e99e1893a60a564bc00719
Salaron <breykin.alexey@gmail.com>
2022-02-07 06:14:54 +0500

    fix(server): палиндром не распознаётся в верхнем регистре

--- Server/Controllers/IsPalindromeController.cs
+++ Server/Controllers/IsPalindromeController.cs
@@ -37,9 +37,11 @@ namespace Server.Controllers
             if (ignoreSymbols)
             {
                 // Убираем лишние символы. Так строка вида "a.ba" может считаться палиндромом
-                var except = "!?.,~ @#$%^&*<>()[]{}:;`'\"\n\r\t";
+                var except = "!?.,~ @#$%^&*<>()[]{}:;`’'\"\n\r\t";
                 str = new string(str.Where(c => !except.Contains(c)).ToArray());
             }
+            // Делаем всё в lowercase, чтобы можно было проверять целые предложения
+            str = str.ToLower();
             for (int i = 0; i < str.Length / 2; i++)
             {
                 if (str[i] != str[str.Length - i - 1]) return false;
